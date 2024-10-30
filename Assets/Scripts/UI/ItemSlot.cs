0f00b88c40c412acb1fd2ab44b32df9f1443ccb8
dearnm <minbongee@gmail.com>
2024-10-30 12:29:21 +0900

    init

---
+++ Assets/Scripts/UI/ItemSlot.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSlot : MonoBehaviour
+{
+    public ItemData item;
+    
+    public UIInventory inventory;
+
+    public int index;
+    public bool equipped;
+    public int quantity;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Set()
+    {
+
+    }
+
+    public void Clear()
+    {
+
+    }
+}
